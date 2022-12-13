e7dc8952c9418c6999fcf2268530e40af6a4330e
sahad-cp <sahadmanu4@gmail.com>
2022-12-13 13:40:19 +0530

    booking

--- hospital/home/views.py
+++ hospital/home/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Departments,Doctors
+from .forms import BookingForm
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
 def about(request):
     return render(request,'about.html')
+
 def booking(request):
-    return render(request,'booking.html')
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+        
+    
+    
+    form = BookingForm()
+    dict_form = {
+        'form': form
+    }
+    return render(request,'booking.html' , dict_form)
+
 def doctors(request):
-    return render(request,'doctors.html')
+    dict_docs = {
+        'doctors': Doctors.objects.all()
+    }
+    return render(request,'doctors.html' , dict_docs)
+
 def contact(request):
     return render(request,'contact.html')
+
 def department(request):
-    return render(request,'department.html')
+    dict_dept ={
+        'dept' : Departments.objects.all()
+    }
+    return render(request,'department.html',dict_dept)
--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/media/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
